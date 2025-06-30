26e1dc13128aabeeff78db95a9f9ca1da879a8bc
MosheOgalbo <65408488+MosheOgalbo@users.noreply.github.com>
2025-06-30 22:41:51 +0300

    feat: add Tailwind CSS imports and enhance styling in App.css and index.css

--- shopping-client/src/App.jsx
+++ shopping-client/src/App.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import ShoppingScreen from './pages/ShoppingScreen';
 import OrderScreen from './pages/OrderScreen';
-import './App.css';
+ import './App.css';
 
 function App() {
   return (
